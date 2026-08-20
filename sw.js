95fa4cd8d30fbe19b1afe200f5816cd6ff61f7eb
yueranwang02 <87096775+yueranwang02@users.noreply.github.com>
2026-08-20 05:36:44 -0400

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'classic-memories-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
