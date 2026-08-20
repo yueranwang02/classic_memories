a029762eb41a711d2584e5b0ad3337947065536f
yueranwang02 <87096775+yueranwang02@users.noreply.github.com>
2026-08-20 05:43:43 -0400

    Refresh offline cache for new layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'classic-memories-v1';
+const CACHE = 'classic-memories-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
